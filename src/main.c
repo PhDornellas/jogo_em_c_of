0dc867828066f8df5d76398db08186082d0f0394
Seu Nome <joaopaulo122005@gmail.com>
2024-11-13 16:00:35 -0500

    corrigindo codigo e adicionando titulo

--- src/main.c
+++ src/main.c
@@ -6,24 +6,6 @@
 #include "keyboard.h"
 #include "timer.h"
 
-void mostrar_resultados(int vitoria, int derrota, int empate) {
-    screenClear();
-    screenInit(1);
-
-    screenGotoxy(22, 8);
-    printf("Você teve um total de:");
-    screenGotoxy(22, 10);
-    printf("%d VITÓRIAS", vitoria);
-    screenGotoxy(22, 12);
-    printf("%d DERROTAS", derrota);
-    screenGotoxy(22, 14);
-    printf("%d EMPATE", empate);
-
-    screenGotoxy(22, 16);
-    printf("Pressione ENTER para sair...");
-    getchar();
-    getchar();
-}
 
 int escolha_opcao() {
     screenClear();
@@ -35,6 +17,7 @@ int escolha_opcao() {
     scanf("%d", &opcao);
     return opcao;
 }
+
 
 void verificacao_da_escolha(int opcao_user, int *n_empate, int *n_vitoria, int *n_derrota) {
     screenClear();
@@ -66,28 +49,72 @@ void verificacao_da_escolha(int opcao_user, int *n_empate, int *n_vitoria, int *
     }
 }
 
+
+void mostrar_resultados(int vitoria, int derrota, int empate) {
+    screenClear();
+    screenInit(1);
+
+    screenGotoxy(22, 8);
+    printf("Você teve um total de:");
+    screenGotoxy(22, 10);
+    printf("%d VITÓRIAS", vitoria);
+    screenGotoxy(22, 12);
+    printf("%d DERROTAS", derrota);
+    screenGotoxy(22, 14);
+    printf("%d EMPATE", empate);
+
+    screenGotoxy(22, 16);
+    printf("Pressione ENTER para sair...");
+    getchar();
+
+}
+
+
 int main() {
-    screenInit(1);  
+
+    screenInit(1);
     keyboardInit();
+
+    screenGotoxy (3,8);
+    printf(" ____        _   _   _        __     __  _    _   _  __  __  _____  _  __ \n");
+    screenGotoxy (3,9);
+    printf("| __ )  __ _| |_| |_| | ___   \\ \\   / / | |  | | | | \\ \\/ / | ____|| |/ / \n");
+    screenGotoxy (3,10);
+    printf("|  _ \\ / _` | __| __| |/ _ \\   \\ \\ / /  | |  | | | |  \\  /  |  _|  | ' /  \n");
+    screenGotoxy(3,11);
+    printf("| |_) | (_| | |_| |_| |  __/    \\ V /   | |__| |_| |  /  \\  | |___ | . \\  \n");
+    screenGotoxy(3,12); ("|____/ \\__,_|\\__|\\__|_|\\___|     \\_/     \\____/(_)_| /_/\\_\\ |_____||_|\\_\\\n");
+    printf("                                                                          \n");
+    screenGotoxy(3,13);
+    printf("\n");
+
+
+    // Mensagem de boas-vindas ou introdução do jogo
+    screenGotoxy(3,14);
+    printf("Bem-vindo ao BATTLE OF LUCK!\n\n");
+    getchar();
+
+    screenClear();
+    screenInit(1);       
 
     int cont_empate = 0, cont_vitoria = 0, cont_derrota = 0;
     char opcao_sair;
 
     do {
-        int opcao_user = escolha_opcao();
-        verificacao_da_escolha(opcao_user, &cont_empate, &cont_vitoria, &cont_derrota);
+    int opcao_user = escolha_opcao();
+    verificacao_da_escolha(opcao_user, &cont_empate, &cont_vitoria, &cont_derrota);
 
-        screenGotoxy(22, 12);
-        printf("Pressione ENTER para continuar...");
-        getchar();
-        getchar();
+    screenGotoxy(22, 12);
+    printf("Pressione ENTER para continuar...");
+    getchar();
+    getchar();
 
-        screenClear();
-        screenInit(1);
+    screenClear();
+    screenInit(1);
 
-        screenGotoxy(22, 12);
-        printf("Deseja continuar? Sim[S] Não[N]: ");
-        scanf(" %c", &opcao_sair);
+    screenGotoxy(22, 12);
+    printf("Deseja continuar? Sim[S] Não[N]: ");
+    scanf(" %c", &opcao_sair);
 
     } while (opcao_sair == 'S' || opcao_sair == 's');
 
@@ -95,5 +122,6 @@ int main() {
 
     keyboardDestroy();
     screenDestroy();
+
     return 0;
 }
